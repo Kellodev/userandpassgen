aabbfca31c9ea46d50e11cf6c22ac9b1488e10b9
Kellodev <149101968+Kellodev@users.noreply.github.com>
2023-10-27 21:02:18 +0300

    idk im bored and just uploading shit to git:)

---
+++ passwordgen.py
@@ -0,0 +1,19 @@
+import random, time
+
+chars = "abcdefghijklmnopgrstuvwxyz1234567890!.?ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+passwordlenght = input("How many characters do you want the password to be?")
+usernamelenght = input("How many characters do you want the username to be?")
+
+password = ""
+username = ""
+
+for i in range(int(passwordlenght)):
+    password += random.choice(chars)
+
+for i in range(int(usernamelenght)):
+    username += random.choice(chars)
+
+print("PASS: " + password + " - USERNAME: " + username)
+
+# closes in cmd idk why lol:D (DONT ASK!)
+time.sleep(99999)
